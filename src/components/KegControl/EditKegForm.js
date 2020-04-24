4ace12169e9f211937f7801d482e18d3d3bffce5
Krista Rutz <rutzkri000@gmail.com>
2020-04-24 11:29:20 -0700

    Add sample default values to mock up edit form

--- src/components/KegControl/EditKegForm.js
+++ src/components/KegControl/EditKegForm.js
@@ -5,7 +5,32 @@ function EditKegForm(props) {
   return (
     <>
       <h3>Edit Keg</h3>
-      <ReusableKegForm />
+      <ReusableKegForm
+        //  formTitle={props.}
+        //  formDescription={props.}
+        //  itemNameDefault={props.}
+        //  descDefault={props.}
+        //  originDefault={props.}
+        //  brewerDefault={props.}
+        //  priceDefault={props.}
+        //  abvDefault={props.}
+        //  ibuDefault={props.}
+        //  id={props.}
+        //  onSubmitFunction={props.}
+        //  submitButtonText={props.}
+        formTitle="Edit mee"
+        formDescription="Example description of how to edit the keg"
+        itemNameDefault="Item Name"
+        descDefault="this is the greatest beer ever brewed"
+        originDefault="Seattle, WA"
+        brewerDefault="Elysian Brewing"
+        priceDefault="7"
+        abvDefault="6.2"
+        ibuDefault="30"
+        id="sample-id"
+        onSubmitFunction=""
+        submitButtonText="save edits"
+      />
     </>
   );
 }
