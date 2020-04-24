21c9d0c86d30c0a676959fe7dd9afe21898209b6
Krista Rutz <rutzkri000@gmail.com>
2020-04-24 12:26:25 -0700

    Attempt to use keg object in form

--- src/components/KegControl/EditKegForm.js
+++ src/components/KegControl/EditKegForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReusableKegForm from "./ReusableKegForm";
+import PropTypes from "prop-types";
 
 function EditKegForm(props) {
   return (
@@ -18,21 +19,44 @@ function EditKegForm(props) {
         //  id={props.}
         //  onSubmitFunction={props.}
         //  submitButtonText={props.}
+
         formTitle="Edit mee"
         formDescription="Example description of how to edit the keg"
-        itemNameDefault="Item Name"
-        descDefault="this is the greatest beer ever brewed"
-        originDefault="Seattle, WA"
-        brewerDefault="Elysian Brewing"
-        priceDefault="7"
-        abvDefault="6.2"
-        ibuDefault="30"
-        id="sample-id"
-        onSubmitFunction=""
+        // itemNameDefault="Item Name"
+        // descDefault="this is the greatest beer ever brewed"
+        // originDefault="Seattle, WA"
+        // brewerDefault="Elysian Brewing"
+        // priceDefault="7"
+        // abvDefault="6.2"
+        // ibuDefault="30"
+        // id="sample-id"
+        keg={sampleKeg}
+        //onSubmitFunction=""
         submitButtonText="save edits"
       />
     </>
   );
 }
+
+const sampleKeg = {
+  itemName: "Superfuzz",
+  desc:
+    "Is a mind-bending whirl of the aromas and flavors of blood orange and Northwest hops. Orange peel added to the whirlpool, and Blood Orange in the fermenter",
+  origin: "Seattle, WA, USA",
+  brewer: "Elysian Brewing",
+  price: 180,
+  abv: 6.4,
+  ibu: 45,
+  id: "sample-id",
+};
+
+ReusableKegForm.propTypes = {
+  formTitle: PropTypes.string,
+  formDescription: PropTypes.string,
+  //potentially put lines 80-87 as a single object and destructure
+  keg: PropTypes.object,
+  onSubmitFunction: PropTypes.func,
+  submitButtonText: PropTypes.string,
+};
 
 export default EditKegForm;
--- src/components/KegControl/ReusableKegForm.js
+++ src/components/KegControl/ReusableKegForm.js
@@ -2,16 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ReusableKegForm(props) {
+  const { formTitle, formDescription, keg, submitButtonText } = props;
+
   const handleClick = (event) => {
     event.preventDefault();
     console.log("clicked");
+  };
+
+  const keg2 = {
+    itemName: "Superfuzz",
+    desc:
+      "Is a mind-bending whirl of the aromas and flavors of blood orange and Northwest hops. Orange peel added to the whirlpool, and Blood Orange in the fermenter",
+    origin: "Seattle, WA, USA",
+    brewer: "Elysian Brewing",
+    price: 180,
+    abv: 6.4,
+    ibu: 45,
+    id: "sample-id",
   };
 
   return (
     <>
       <h2>{props.formTitle}</h2>
       <p className="lead">{props.formDescription}</p>
-      <form>
+      {/* <form>
         <div className="form-group">
           <input
             type="text"
@@ -70,6 +84,66 @@ function ReusableKegForm(props) {
         <button type="submit" onClick={handleClick}>
           {props.submitButtonText}
         </button>
+      </form> */}
+      <form>
+        <div className="form-group">
+          <input
+            type="text"
+            name="itemName"
+            defaultValue={keg.itemName}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="desc"
+            defaultValue={keg.desc}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="origin"
+            defaultValue={keg.origin}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="brewer"
+            defaultValue={keg.brewer}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="number"
+            name="price"
+            defaultValue={keg.price}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="number"
+            name="abv"
+            defaultValue={keg.abv}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="number"
+            name="ibu"
+            defaultValue={keg.ibu}
+            className="form-control"
+          />
+        </div>
+        <button type="submit" onClick={handleClick}>
+          {props.submitButtonText}
+        </button>
       </form>
     </>
   );
@@ -79,15 +153,16 @@ ReusableKegForm.propTypes = {
   formTitle: PropTypes.string,
   formDescription: PropTypes.string,
   //potentially put lines 80-87 as a single object and destructure
-  itemNameDefault: PropTypes.string,
-  descDefault: PropTypes.string,
-  originDefault: PropTypes.string,
-  brewerDefault: PropTypes.string,
-  priceDefault: PropTypes.number,
-  abvDefault: PropTypes.number,
-  ibuDefault: PropTypes.number,
-  id: PropTypes.string,
-  onSubmitFunction: PropTypes.func,
+  // itemNameDefault: PropTypes.string,
+  // descDefault: PropTypes.string,
+  // originDefault: PropTypes.string,
+  // brewerDefault: PropTypes.string,
+  // priceDefault: PropTypes.number,
+  // abvDefault: PropTypes.number,
+  // ibuDefault: PropTypes.number,
+  // id: PropTypes.string,
+  keg: PropTypes.object,
+  // onSubmitFunction: PropTypes.func,
   submitButtonText: PropTypes.string,
 };
 
