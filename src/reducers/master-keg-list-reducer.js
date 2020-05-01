516fc4ea21678ca217512cf99c1168ed99329764
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 09:11:02 -0700

    Add code for delete keg case

--- src/reducers/master-keg-list-reducer.js
+++ src/reducers/master-keg-list-reducer.js
@@ -4,6 +4,10 @@ export default (state = {}, action) => {
   switch (action.type) {
     case c.ADD_NEW_KEG:
       return Object.assign({}, state, { [action.payload.id]: action.payload });
+    case c.DELETE_KEG:
+      const newState = { ...state };
+      delete newState[action.id];
+      return newState;
     default:
       return state;
   }
