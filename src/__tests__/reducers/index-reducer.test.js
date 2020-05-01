81ebe976b0b23613f5b7cc04cc64f7598f8e1bf6
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 12:02:21 -0700

    Add 'sanity checks' to ensure root reducer returns default state of each reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from "../../reducers";
+import { createStore } from "redux";
+import selectedKegReducer from "../../reducers/selected-keg-reducer";
+import masterKegListReducer from "../../reducers/master-keg-list-reducer";
+
+const store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   test("should return default state if there is no action type recognized", () => {
@@ -6,5 +11,17 @@ describe("rootReducer", () => {
       masterKegList: {},
       selectedKeg: null,
     });
+  });
+
+  it("initial state of masterKegListReducer matches rootReducer", () => {
+    expect(store.getState().masterKegList).toEqual(
+      masterKegListReducer(undefined, { type: null })
+    );
+  });
+
+  it("initial state of selectKegReducer matches rootReducer", () => {
+    expect(store.getState().selectedKeg).toEqual(
+      selectedKegReducer(undefined, { type: null })
+    );
   });
 });
