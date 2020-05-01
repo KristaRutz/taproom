5b70ef288ca991ae2a5a4398244c97b7fd37a51f
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 12:12:07 -0700

    test to ensure that stored state reflects the dispatches using a rootReducer properly

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from "../../reducers";
 import { createStore } from "redux";
 import selectedKegReducer from "../../reducers/selected-keg-reducer";
 import masterKegListReducer from "../../reducers/master-keg-list-reducer";
+import * as a from "../../actions";
 
 const store = createStore(rootReducer);
 
@@ -22,6 +23,22 @@ describe("rootReducer", () => {
   it("initial state of selectKegReducer matches rootReducer", () => {
     expect(store.getState().selectedKeg).toEqual(
       selectedKegReducer(undefined, { type: null })
+    );
+  });
+
+  it("updated state of masterKegListReducer matches rootReducer", () => {
+    const action = a.addKeg({ id: "test" });
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(
+      masterKegListReducer(undefined, action)
+    );
+  });
+
+  it("updated state of selectKegReducer matches rootReducer", () => {
+    const action = a.selectKeg({ id: "test" });
+    store.dispatch(action);
+    expect(store.getState().selectedKeg).toEqual(
+      selectedKegReducer(undefined, action)
     );
   });
 });
