3bcd797cdae68e27b1ee4c3062f778564c1ed35b
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 14:19:05 -0700

    Include constants and methods for add/edit forms

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,31 @@
 import * as c from "./ActionTypes";
 
+/* masterKegList actions */
 export const addKeg = (keg) => ({
   type: c.ADD_NEW_KEG,
   keg,
 });
-
 export const editKeg = addKeg;
-
 export const deleteKeg = (id) => ({
   type: c.DELETE_KEG,
   id,
 });
 
+/* selectedKeg actions */
 export const selectKeg = (keg) => ({
   type: c.SELECT_KEG,
   keg,
 });
-
 export const deselectKeg = () => ({
   type: c.DESELECT_KEG,
+});
+
+/* addKegForm actions */
+export const toggleAddKegForm = () => ({
+  type: c.TOGGLE_ADD_KEG_FORM,
+});
+
+/* editKegForm actions */
+export const toggleEditKegForm = () => ({
+  type: c.TOGGLE_EDIT_KEG_FORM,
 });
