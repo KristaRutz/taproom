ad08baa648429be3aa76469b2c95b705ef458187
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 11:27:48 -0700

    test deselection of keg

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -4,16 +4,7 @@ import { v4 } from "uuid";
 
 describe("selectedKegReducer", () => {
   test("should return default state if the action type passed into the reducer is not recognized", () => {
-    expect(
-      selectedKegReducer(
-        {
-          selectedKeg: null,
-        },
-        { type: null }
-      )
-    ).toEqual({
-      selectedKeg: null,
-    });
+    expect(selectedKegReducer(null, { type: null })).toEqual(null);
   });
 
   test("should return {selectedKeg: null} in DESELECT_KEG action", () => {
@@ -21,10 +12,6 @@ describe("selectedKegReducer", () => {
       itemName: test,
       id: v4(),
     };
-    expect(
-      selectedKegReducer({ selectedKeg: testKeg }, c.DESELECT_KEG)
-    ).toEqual({
-      selectedKeg: null,
-    });
+    expect(selectedKegReducer(testKeg, { type: c.DESELECT_KEG })).toEqual(null);
   });
 });
