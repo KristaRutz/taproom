daeb5df25f91a1f435908d629be856c91d940e2c
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 14:29:52 -0700

    test the addForm/editForm action creators

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -3,6 +3,7 @@ import * as c from "../../actions/ActionTypes";
 import { v4 } from "uuid";
 
 describe("taproom action types", () => {
+  /* masterKegList actions */
   it("addKeg should create ADD_KEG action", () => {
     const testKeg = { itemName: "Example" };
     expect(actions.addKeg(testKeg)).toEqual({
@@ -10,7 +11,6 @@ describe("taproom action types", () => {
       keg: testKeg,
     });
   });
-
   it("editKeg should create ADD_KEG action", () => {
     const testKeg = { itemName: "Example" };
     expect(actions.editKeg(testKeg)).toEqual({
@@ -18,12 +18,12 @@ describe("taproom action types", () => {
       keg: testKeg,
     });
   });
-
   it("deleteKeg should create a DELETE_KEG action", () => {
     const id = v4();
     expect(actions.deleteKeg(id)).toEqual({ type: c.DELETE_KEG, id: id });
   });
 
+  /* selectedKeg actions */
   it("selectKeg should create a SELECT_KEG action", () => {
     const testKeg = { itemName: "Example" };
     expect(actions.selectKeg(testKeg)).toEqual({
@@ -31,10 +31,23 @@ describe("taproom action types", () => {
       keg: testKeg,
     });
   });
-
   it("deselectKeg should create a DESELECT_KEG action", () => {
     expect(actions.deselectKeg()).toEqual({
       type: c.DESELECT_KEG,
+    });
+  });
+
+  /* addKegForm actions */
+  it("toggleAddKegForm should create a TOGGLE_ADD_KEG_FORM action", () => {
+    expect(actions.toggleAddKegForm()).toEqual({
+      type: c.TOGGLE_ADD_KEG_FORM,
+    });
+  });
+
+  /* editKegForm actions */
+  it("toggleEditKegForm should create a TOGGLE_EDIT_KEG_FORM action", () => {
+    expect(actions.toggleEditKegForm()).toEqual({
+      type: c.TOGGLE_EDIT_KEG_FORM,
     });
   });
 });
