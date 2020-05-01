95e0eac9241878912b2ee0ec7605c7ac2a749a08
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 14:17:37 -0700

    test for default state and form toggling

---
+++ src/__tests__/reducers/edit-keg-form-on-reducer.test.js
@@ -0,0 +1,17 @@
+import * as c from "../../actions/ActionTypes";
+import editKegFormOnReducer from "../../reducers/edit-keg-form-on-reducer";
+
+describe("editKegFormReducer", () => {
+  test("should return default state if the action type passed into the reducer is not recognized", () => {
+    expect(editKegFormOnReducer(false, { type: null })).toEqual(false);
+  });
+
+  test("should toggle form visibility state", () => {
+    expect(
+      editKegFormOnReducer(false, { type: c.TOGGLE_EDIT_KEG_FORM })
+    ).toEqual(true);
+    expect(
+      editKegFormOnReducer(true, { type: c.TOGGLE_EDIT_KEG_FORM })
+    ).toEqual(false);
+  });
+});
