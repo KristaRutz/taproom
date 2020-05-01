188b5787f2295da7205eb0631b1ce61dce9e9a70
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 11:29:59 -0700

    Add code paths to select and deselect kegs

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -1,3 +1,12 @@
-export default (state = { selectedKeg: null }, action) => {
-  return state;
+import * as c from "../actions/ActionTypes";
+
+export default (state = null, action) => {
+  switch (action.type) {
+    case c.DESELECT_KEG:
+      return null;
+    case c.SELECT_KEG:
+      return action.keg;
+    default:
+      return state;
+  }
 };
