546b3aaeeba57f955e2d01208841d220a2de90f0
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 14:39:30 -0700

    Update rootReducer to contain all 4 state slices

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import masterKegListReducer from "./master-keg-list-reducer";
 import selectedKegReducer from "./selected-keg-reducer";
+import addKegFormOnReducer from "./add-keg-form-on-reducer";
+import editKegFormOnReducer from "./edit-keg-form-on-reducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   masterKegList: masterKegListReducer,
   selectedKeg: selectedKegReducer,
+  addKegFormOn: addKegFormOnReducer,
+  editKegFormOn: editKegFormOnReducer,
 });
 
 export default rootReducer;
