c01a084c5f6e6bde5a42254fb1588bf9ed0cc935
Krista Rutz <rutzkri000@gmail.com>
2020-05-01 11:11:10 -0700

    Test to assure DESELECT_KEG sets selectedKeg: null

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,5 +1,6 @@
 import selectedKegReducer from "../../reducers/selected-keg-reducer";
 import * as c from "../../actions/ActionTypes";
+import { v4 } from "uuid";
 
 describe("selectedKegReducer", () => {
   test("should return default state if the action type passed into the reducer is not recognized", () => {
@@ -10,6 +11,18 @@ describe("selectedKegReducer", () => {
         },
         { type: null }
       )
+    ).toEqual({
+      selectedKeg: null,
+    });
+  });
+
+  test("should return {selectedKeg: null} in DESELECT_KEG action", () => {
+    const testKeg = {
+      itemName: test,
+      id: v4(),
+    };
+    expect(
+      selectedKegReducer({ selectedKeg: testKeg }, c.DESELECT_KEG)
     ).toEqual({
       selectedKeg: null,
     });
--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,2 +1,4 @@
 export const ADD_NEW_KEG = "ADD_NEW_KEG";
 export const DELETE_KEG = "DELETE_KEG";
+export const SELECT_KEG = "SELECT_KEG";
+export const DESELECT_KEG = "DESELECT_KEG";
